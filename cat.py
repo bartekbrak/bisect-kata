4a37fe34508f8cae5befa620e284343f61256cdb
Lawanda Holcomb <dekbsebhpijfuvivpmuc@gmail.com>
2014-07-18 11:36:50 +0200

    qlsitjaxvbckdqjhfzzh

---
+++ cat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import base64
+
+
+animal = ('G1s5M20KICAgICAgICAgICAgICAgICAgICAgICAgXwogICAgICAgICAgICAgICAgICAgICAgIHwg\nXAogICAgICAgICAgICAgICAgICAgIC'
+          'AgIHwgfAogICAgICAgICAgICAgICAgICAgICAgIHwgfAog\nIHxcICAgICAgICAgICAgICAgICAgIHwgfAogLywgflwgICAgICAgICAgICAg'
+          'ICAgLyAvClggICAg\nIGAtLi4uLi4tLS0tLS0tLi8gLwogfi0uIH4gIH4gICAgICAgICAgICAgIHwKICAgIFwgICAgICAg\nICAgICAgLyAg'
+          'ICB8CiAgICAgXCAgL18gICAgIF9fX1wgICAvCiAgICAgfCAvXCB+fn5+fiAgIFwg\nfAogICAgIHwgfCBcICAgICAgICB8fCB8CiAgICAgfC'
+          'B8XCBcICAgICAgIHx8ICkKICAgIChfLyAo\nXy8gICAgICAoKF8vG1swbQ==\n')
+
+print base64.decodestring(animal), 'prrr, prr... all is good'
